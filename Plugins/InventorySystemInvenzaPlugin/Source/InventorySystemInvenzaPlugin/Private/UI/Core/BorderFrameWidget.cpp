fb8c0a8f2e23ed0793d04e0fdbf080df441c228b
Alexey <nublin1@yandex.ru>
2026-06-16 22:35:30 +0300

    wip

---
+++ Plugins/InventorySystemInvenzaPlugin/Source/InventorySystemInvenzaPlugin/Private/UI/Core/BorderFrameWidget.cpp
@@ -0,0 +1,30 @@
+﻿// Nublin Studio 2026 All Rights Reserved.
+
+
+#include "UI/Core/BorderFrameWidget.h"
+
+#include "Components/Border.h"
+
+UBorderFrameWidget::UBorderFrameWidget()
+{
+}
+
+void UBorderFrameWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	
+	auto ApplyBorderStyle = [](UBorder* Border, const FBorderFrameStyle& Style)
+	{
+		if (!Border) return;
+
+		Border->SetBrushColor(Style.BrushColor);
+		Border->SetBrush(Style.Brush);
+		Border->SetPadding(Style.Padding);
+		Border->SetDesiredSizeScale(Style.DesiredSize);
+	};
+
+	ApplyBorderStyle(TopBorder,    TopBorderStyle);
+	ApplyBorderStyle(BottomBorder, BottomBorderStyle);
+	ApplyBorderStyle(LeftBorder,   LeftBorderStyle);
+	ApplyBorderStyle(RightBorder,  RightBorderStyle);
+}
---
+++ Plugins/InventorySystemInvenzaPlugin/Source/InventorySystemInvenzaPlugin/Public/UI/Core/BorderFrameWidget.h
@@ -0,0 +1,73 @@
+﻿// Nublin Studio 2026 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/InvenzaBaseWidget.h"
+#include "BorderFrameWidget.generated.h"
+
+class UBorder;
+
+USTRUCT(BlueprintType)
+struct FBorderFrameStyle
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FSlateBrush Brush;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLinearColor BrushColor = FLinearColor::White;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FMargin Padding;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector2D DesiredSize = FVector2D(1.f);
+};
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEMINVENZAPLUGIN_API UBorderFrameWidget : public UInvenzaBaseWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UBorderFrameWidget();
+	
+	virtual void NativePreConstruct() override;
+	
+	//====================================================================
+	// PROPERTIES AND VARIABLES
+	//====================================================================
+	// Widgets
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UBorder> TopBorder;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UBorder> BottomBorder;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UBorder> LeftBorder;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UBorder> RightBorder;
+	
+	UPROPERTY(meta=(BindWidgetOptional))
+	TObjectPtr<UNamedSlot> NamedSlot;
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Top Border")
+	FBorderFrameStyle TopBorderStyle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bottom Border")
+	FBorderFrameStyle BottomBorderStyle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Left Border")
+	FBorderFrameStyle LeftBorderStyle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Right Border")
+	FBorderFrameStyle RightBorderStyle;
+};
